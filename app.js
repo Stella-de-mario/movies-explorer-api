df22f2548da3ea80dfc2d8d27b3b25902fa3bc86
Stella-de-mario <irina17otr@gmail.com>
2022-10-15 01:27:31 +0500

    limit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
 
 const { errors } = require('celebrate');
 const cors = require('./middlewares/cors');
@@ -10,12 +11,16 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { login, createUser, logOut } = require('./controllers/users');
 const { validateLogin, validateCreateUser } = require('./utils/constants');
 const auth = require('./middlewares/auth');
+const limiter = require('./middlewares/rateLimit');
 const NotFoundError = require('./utils/errors/NotFoundError');
 const errorHandler = require('./middlewares/errorHandler');
 
 const { PORT = 3000, NODE_ENV, MONGO_URL } = process.env;
 const app = express();
 
+app.use(limiter);
+
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
